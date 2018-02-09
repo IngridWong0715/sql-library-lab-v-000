0c4c3aed52584327477a4e36b8465e66a2a84595
Ingrid Wong <yw340@georgetown.edu>
2018-02-09 02:30:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters HAVING count = MAX(characters.species) GROUP BY species;"
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY species HAVING count = MAX(characters.species) ;"
 end
 
 def select_name_and_series_su
