bac53114b40f6c1dc08ace2ae64d0256012098a2
Ingrid Wong <yw340@georgetown.edu>
2018-02-09 02:49:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,8 +19,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series JOIN characters ON "
 
-  most human characters: 
-  - MAX(COUNT(characters.species='human'))
+  most human characters:
+  - mosthumanseriesid = (SELECT series_id FROM characters MAX(COUNT(characters.species='human'))
 end
 
 def select_character_names_and_number_of_books_they_are_in
