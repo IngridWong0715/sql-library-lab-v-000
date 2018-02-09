5c801d2dda12c0ac70197bd89d5f3024f4e727bc
Ingrid Wong <yw340@georgetown.edu>
2018-02-09 02:23:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   #"SELECT name, motto FROM characters WHERE motto = (SELECT MAX(LENGTH(motto)) FROM characters);"
-    "SELECT name, motto FROM characters ORDER BY LENGTH(motto);"
+    "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
