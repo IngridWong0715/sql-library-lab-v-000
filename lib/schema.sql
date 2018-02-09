c7debf2588e26aa8afacb46d15c6cc151c948400
Ingrid Wong <yw340@georgetown.edu>
2018-02-09 01:55:49 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE series(
   subgenre_id INTEGER
 );
 
-CREATE TABLE sub_genre(
+CREATE TABLE subgenres(
   id INTEGER PRIMARY KEY,
   name TEXT
 );
@@ -34,5 +34,5 @@ CREATE TABLE characters(
 CREATE TABLE character_books(
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
-  sbook_id INTEGER
+  book_id INTEGER
 );
